6be0c2197eaefc081f3f07077797c943ce60402e
mwester117 <42421851+mwester117@users.noreply.github.com>
2023-09-28 16:52:36 +0200

    Updated copyright year

--- plugins/modules/sentinelone_path_exclusions.py
+++ plugins/modules/sentinelone_path_exclusions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2022, Marco Wester <marco.wester@sva.de>, Lasse Wackers <lasse.wackers@sva.de>
+# Copyright: (c) 2023, Marco Wester <marco.wester@sva.de>, Lasse Wackers <lasse.wackers@sva.de>
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
